27b574c51abd43ae0863947a6077df250ecc1fe8
doublequan <msa_quanquan@163.com>
2015-08-07 11:48:03 +0800

    test of php development enviroment
    test the php development enviroment
    apache 2.4.4 + php 5.4.5 + mySQL 5.5..13

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php       
+$con=mysql_connect('127.0.0.1','root','1234'); //user为用户名password为密码
+if(!$con)       
+{       
+echo "fail";       
+}       
+else      
+{       
+echo "success";       
+}       
+?>
